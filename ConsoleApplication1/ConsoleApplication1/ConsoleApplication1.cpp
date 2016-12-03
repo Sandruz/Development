925789ca9cdd63d3fcc5959f1de60b87a7d1041d
Sandruz <sandruz.alba@gmail.com>
2016-12-03 11:36:54 +0100

    Commit di prova

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+
+	std::cout << "Test \n";
+
+	system("Pause");
+    return 0;
+}
+
